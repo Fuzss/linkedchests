be4d1b327defd48ad96510c514e4aa2f9019cb0c
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-07-13 09:58:46 +0200

    sync recipe serializer on fabric

--- Fabric/src/main/java/fuzs/linkedchests/fabric/LinkedChestsFabric.java
+++ Fabric/src/main/java/fuzs/linkedchests/fabric/LinkedChestsFabric.java
@@ -1,13 +1,16 @@
 package fuzs.linkedchests.fabric;
 
 import fuzs.linkedchests.common.LinkedChests;
+import fuzs.linkedchests.common.init.ModRegistry;
 import fuzs.puzzleslib.common.api.core.v1.ModConstructor;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.recipe.v1.sync.RecipeSynchronization;
 
 public class LinkedChestsFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
         ModConstructor.construct(LinkedChests.MOD_ID, LinkedChests::new);
+        RecipeSynchronization.synchronizeRecipeSerializer(ModRegistry.SHAPED_DYE_CHANNEL_RECIPE_SERIALIZER.value());
     }
 }
